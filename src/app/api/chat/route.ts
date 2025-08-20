eb0f758252625680be6c3bb50b372cc81fcfbde7
Yusuke Tanaka <wing0920@gmail.com>
2025-08-20 19:25:44 +0900

    add a weather tool

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -1,5 +1,12 @@
 import { anthropic } from "@ai-sdk/anthropic";
-import { convertToModelMessages, streamText, UIMessage } from "ai";
+import {
+  convertToModelMessages,
+  stepCountIs,
+  streamText,
+  tool,
+  UIMessage,
+} from "ai";
+import { z } from "zod";
 
 // Allow streaming responses up to 30 seconds
 export const maxDuration = 30;
@@ -10,6 +17,23 @@ export async function POST(req: Request) {
   const result = streamText({
     model: anthropic("claude-sonnet-4-20250514"),
     messages: convertToModelMessages(messages),
+    stopWhen: stepCountIs(10),
+    tools: {
+      weather: tool({
+        description: "Get the weather in a location (celsius)",
+        inputSchema: z.object({
+          location: z.string().describe("The location to get the weather for"),
+        }),
+        execute: async ({ location }) => {
+          // Return a fake value
+          const temperature = Math.round(Math.random() * (40 - (-10)) + (-10));
+          return {
+            location,
+            temperature,
+          };
+        },
+      }),
+    },
   });
 
   return result.toUIMessageStreamResponse();
