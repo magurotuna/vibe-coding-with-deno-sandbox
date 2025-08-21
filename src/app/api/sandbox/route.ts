d0844503f0c34633498a44672b6a79caa430d21e
Yusuke Tanaka <wing0920@gmail.com>
2025-08-21 17:13:06 +0900

    fmt

--- src/app/api/sandbox/route.ts
+++ src/app/api/sandbox/route.ts
@@ -1,6 +1,6 @@
 import { createSandbox } from "@/app/sandbox-map";
 
 export async function POST(req: Request) {
-    const [sandboxId, _sandbox] = await createSandbox();
-    return new Response(sandboxId);
+  const [sandboxId, _sandbox] = await createSandbox();
+  return new Response(sandboxId);
 }
